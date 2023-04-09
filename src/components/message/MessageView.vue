755d2bac04ab796ffc3328bf5b314423d93aeac7
zuozijian3720 <zuozijian1994@gmail.com>
2023-04-10 00:26:50 +0800

    fix: delete debug code

--- src/components/message/MessageView.vue
+++ src/components/message/MessageView.vue
@@ -145,8 +145,7 @@ watch(hover, (hover) => {
           {{ message.message.content }}
         </div>
         <span v-else whitespace="pre-wrap"
-          >{{ message.message.content }}{{ message.message.content
-          }}{{ message.message.content }}<span inline-block h="1em"></span
+          >{{ message.message.content }}<span inline-block h="1em"></span
         ></span>
         <span class="ping" v-if="message.abort" text-gray-300></span>
       </div>
