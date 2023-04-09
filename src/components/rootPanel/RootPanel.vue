d5f1179e89763036fa950670feadf19b9e2e5f88
zuozijian3720 <zuozijian1994@gmail.com>
2023-04-09 22:26:13 +0800

    feat: add title bar to root panel

--- src/components/rootPanel/RootPanel.vue
+++ src/components/rootPanel/RootPanel.vue
@@ -1,5 +1,8 @@
 <template>
   <div flex flex-col bg-gray-100 overflow-hidden h-full>
+    <div flex justify-center p2>
+      <div font-bold select-none>Chat 记录</div>
+    </div>
     <div overflow-y-scroll overflow-x-hidden flex-1 flex flex-col p-2>
       <ChatBlock
         :class="[i != 0 && 'mt-2']"
