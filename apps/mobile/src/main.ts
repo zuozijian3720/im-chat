52acd6824ec0a86df1c99aa5600236dc8c61d71d
zuozijian3720 <zuozijian1994@gmail.com>
2023-04-12 18:14:44 +0800

    fix: back button will back twice

--- apps/mobile/src/main.ts
+++ apps/mobile/src/main.ts
@@ -4,7 +4,6 @@ import "./main.scss";
 import AppView from "./App.vue";
 import { IonicVue, isPlatform } from "@ionic/vue";
 import { router } from "./router";
-import { App } from "@capacitor/app";
 import { StatusBar, Style } from "@capacitor/status-bar";
 import VConsole from "vconsole";
 
@@ -15,13 +14,6 @@ if (!isPlatform("mobileweb")) {
   StatusBar.setBackgroundColor({ color: "#f3f4f6" });
   StatusBar.setStyle({ style: Style.Light });
 }
-App.addListener("backButton", (event) => {
-  if (event.canGoBack) {
-    router.back();
-  } else {
-    App.minimizeApp();
-  }
-});
 const app = createApp(AppView).use(IonicVue).use(router);
 router.isReady().then(() => {
   app.mount("#app");
